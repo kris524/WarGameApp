9bded8eeb393cff0f88fd46ff400a3533553a6b1
Kris <kristian_dilov@abv.bg>
2024-07-20 17:18:11 +0100

    Initial Commit

---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by Kristiyan Dilov on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
