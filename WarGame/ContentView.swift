97ede333f267349f786af41956c75e0a9e3ad7e6
Kris <kristian_dilov@abv.bg>
2024-07-20 23:04:42 +0100

    Add UI layout

--- WarGame/ContentView.swift
+++ WarGame/ContentView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Image("logo")
+            Spacer()
+            
+            HStack {
+                Image("card2")
+                Spacer()
+                Image("card3")
+            }
+            Spacer()
+            Image("button")
+            Spacer()
+            
+            HStack {
+                VStack {
+                    Text("Player")
+                        
+                    Text("0")
+                }.font(.title)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                VStack {
+                    Text("CPU")
+                       
+                    Text("0")
+                }.font(.title)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.trailing)
+            }
+                        
+        }.background(Image("background-plain"))
+            .padding()
     }
 }
  
