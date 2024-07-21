f52c593ec2aa6bcc67e3bc92b42b75300f83d2d4
Kris <kristian_dilov@abv.bg>
2024-07-21 10:41:00 +0100

    Add button logic

--- WarGame/ContentView.swift
+++ WarGame/ContentView.swift
@@ -8,40 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var playerCard = "card7"
+    var cpuCard = "card8"
+    
+    var playerScore = 0
+    var cpuScore = 0
+    
     var body: some View {
+        
+
         VStack {
             Image("logo")
             Spacer()
             
             HStack {
-                Image("card2")
+                
+                Image(playerCard)
                 Spacer()
-                Image("card3")
+                Image(cpuCard)
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Spacer()
+                
+            Button{ 
+                deal()
+            }
+            label: {
+                Image("button")
             }
             Spacer()
-            Image("button")
-            Spacer()
             
-            HStack {
+            HStack() {
                 VStack {
                     Text("Player")
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                         
-                    Text("0")
+                    Text("\(playerScore)")
                 }.font(.title)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
                 Spacer()
                 VStack {
                     Text("CPU")
-                       
-                    Text("0")
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    Text("\(cpuScore)")
                 }.font(.title)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.trailing)
             }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                         
         }.background(Image("background-plain"))
             .padding()
+    }
+    
+    func deal () {
+        print("ASD")
     }
 }
  
